434c00fef8d76e6862e920276011697f231ce6b9
xmarimarquesh <marimarquesh39@gmail.com>
2024-11-13 09:31:22 -0300

    primeiro commit da mari

---
+++ src/main/java/com/example/demo/dto/Token.java
@@ -0,0 +1,25 @@
+package com.example.demo.dto;
+
+public class Token {
+    
+    private Long id;
+
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
