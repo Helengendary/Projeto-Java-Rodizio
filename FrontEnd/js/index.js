9784dd695cefc63b5058bafff2e3efc95eaba12e
CrCardd <cris.rc377@gmail.com>
2024-11-22 09:03:51 -0300

    cors configuration

---
+++ FrontEnd/js/index.js
@@ -0,0 +1,31 @@
+const login_button = document.getElementById('login_button')
+const edv = document.getElementById('edv')
+const password = document.getElementById('pass')
+
+
+login_button.addEventListener('click', async() => {
+    if(edv.value == '' || password == '')
+        return
+
+
+
+    const response = await fetch(
+        'http://localhost:8080/auth',
+        {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    edv: edv.value,
+                    password: password.value
+                }
+            )
+        }
+    )
+
+    const content = await response.json()
+    console.log(content)
+})
